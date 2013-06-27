46b77306c80981081fc3884b02ba10b88591daef
CrixsB <brandon@brandonmcinnis.com>
2013-06-27 18:47:23 +0900

    added sql table info

---
+++ users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username varchar(50) NOT NULL CONSTRAINT username_unique UNIQUE,
+	password varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	join_date datetime NOT NULL,
+)
