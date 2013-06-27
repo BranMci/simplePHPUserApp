0b66fc22269d592581c5935244fe4fb2da860a05
CrixsB <brandon@brandonmcinnis.com>
2013-06-27 18:55:49 +0900

    sql -> mySQL

--- users.sql
+++ users.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users (
 	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	username varchar(50) NOT NULL CONSTRAINT username_unique UNIQUE,
+	username varchar(50) NOT NULL,
 	password varchar(50) NOT NULL,
 	email varchar(50) NOT NULL,
 	join_date datetime NOT NULL,
-)
+	UNIQUE(username)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
